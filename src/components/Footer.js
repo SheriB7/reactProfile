3089b6b2180f962981526a7432bb5268b55f5dbc
Sheri Brown <sherib70@gmail.com>
2022-07-20 14:00:27 -0400

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,28 +2,28 @@ import React from "react";
 import "../styles/footer.css";
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="content has-text-spaced">
-                <a
-                    className="footText1"
-                    href="https://github.com/SheriB7"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    *Github**
-                </a>
-                <a
-                    className="footText2"
-                    href="https://www.linkedin.com/in/sheri-brown-0b012042/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    *LinkedIn*
-                </a>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="content has-text-spaced">
+        <a
+          className="footText1"
+          href="https://github.com/SheriB7"
+          target="_blank"
+          rel="noreferrer"
+        >
+          *Github**
+        </a>
+        <a
+          className="footText2"
+          href="https://www.linkedin.com/in/sheri-brown-0b012042/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          *LinkedIn*
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
